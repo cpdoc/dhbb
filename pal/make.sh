95c310d6b545859a5526c3e34ea14c9f32f8e30c
Alexandre Rademaker <arademaker@gmail.com>
2017-07-19 16:11:49 -0300

    fixing encoding

--- pal/make.sh
+++ pal/make.sh
@@ -1,7 +1,7 @@
 
-for f in $(ls ../text/{1..100}.text); do
+for f in $(ls ../text/*.text); do
     BASE=$(basename $f .text)
     awk 'BEGIN { text=0; } text>1 {print} /^---$/ { text = text + 1; }' $f > $BASE.raw ;
     cat $BASE.raw | /opt/palavras/por.pl --role > $BASE.role ;
-    cat $BASE.role | /opt/palavras/bin/visldep2malt.pl --lang pt | /opt/palavras/bin/extra2sem > $BASE.malt ;
+    # cat $BASE.role | /opt/palavras/bin/visldep2malt.pl --lang pt | /opt/palavras/bin/extra2sem > $BASE.malt ;
 done
