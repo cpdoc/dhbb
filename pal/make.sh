42c481a06145e69dc8a15a53d29a942e2525636f
Alexandre Rademaker <arademaker@gmail.com>
2017-07-25 08:35:39 -0300

    movendo dados de processamento para dhbb-nlp

--- pal/make.sh
+++
@@ -1,8 +0,0 @@
-
-for f in ../raw/*.raw; do
-    BASE=$(basename $f .raw)
-    cat ../raw/$BASE.raw | /opt/palavras/por.pl --role > $BASE.tmp ;
-    awk -f clean.awk $BASE.tmp > $BASE.role;
-    rm $BASE.tmp;
-    # cat $BASE.role | /opt/palavras/bin/visldep2malt.pl --lang pt | /opt/palavras/bin/extra2sem > $BASE.malt ;
-done
--- wks/convert-dict.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo lemma,poscode,surface
-while read f; do echo $f,3,$f; done < $1
