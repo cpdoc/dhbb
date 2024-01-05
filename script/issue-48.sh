8abab335b4f5e16567aa3b94fc0be32eb4fef918
Alexandre Rademaker <arademaker@gmail.com>
2024-01-04 22:04:50 -0300

    related to #48

--- script/issue-48.sh
+++ script/issue-48.sh
@@ -1,5 +1,6 @@
 for f in *.text; do
-    sed -i '' -e 's/[[:<:]]assembléia[[:>:]]/assembleia/g' \
+    sed -i '' \
+	-e 's/[[:<:]]assembléia[[:>:]]/assembleia/g' \
 	-e 's/[[:<:]]assembléias[[:>:]]/assembleias/g' \
 	-e 's/[[:<:]]Assembléia[[:>:]]/Assembleia/g' \
 	-e 's/[[:<:]]Assembléias[[:>:]]/Assembleias/g' \
@@ -9,6 +10,15 @@ for f in *.text; do
 	-e 's/[[:<:]]Idéias[[:>:]]/Ideias/g' \
 	-e 's/[[:<:]]conseqüente[[:>:]]/consequente/g' \
 	-e 's/[[:<:]]freqüentou[[:>:]]/frequentou/g' \
+	-e 's/[[:<:]]freqüencia[[:>:]]/frequencia/g' \
+	-e 's/[[:<:]]freqüencias[[:>:]]/frequencias/g' \
+	-e 's/[[:<:]]freqüente[[:>:]]/frequente/g' \
+	-e 's/[[:<:]]freqüentes[[:>:]]/frequentes/g' \
+	-e 's/[[:<:]]freqüentemente[[:>:]]/frequentemente/g' \
 	-e 's/[[:<:]]conseqüência[[:>:]]/consequencia/g' \
-	-e 's/[[:<:]]conseqüências[[:>:]]/consequencias/g' $f;
+	-e 's/[[:<:]]conseqüências[[:>:]]/consequencias/g' \
+	-e 's/[[:<:]]conseqüentemente[[:>:]]/consequentemente/g' \
+	-e 's/[[:<:]]co-autor[[:>:]]/coautor/g' \
+	-e 's/[[:<:]]co-autoria[[:>:]]/coautoria/g' \
+	-e 's/[[:<:]]apóia[[:>:]]/apoia/g' $f;
 done
